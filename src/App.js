26afce579069ed80999aef428ad013e0795ac98f
Sergio D'Elia <sergio.delia@yahoo.it>
2021-11-04 23:25:41 +0100

    Primo step grafica

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const API = 'https://jsonplaceholder.typicode.com/photos';
+  const [photos, setPhotos] = useState([])
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Albums</h1>
+      <ul>
+      {
+        photos.map(photo => <li key={photo.id}>
+          <img src={photo.url} alt={photo.title} />
+        </li>)
+      }
+      </ul>
+      {API}
     </div>
   );
 }
